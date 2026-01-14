dccc8e2c3e24069368c70be2a8ce5ef2001f9979
saicoder33 <saidighe731@gmail.com>
2026-01-14 09:50:28 +0530

    Nested if-else

---
+++ UNIT2PRG1.py
@@ -0,0 +1,12 @@
+#WRITE A PYTHON PROGRAM TO PRINT LARGEST NUMBER OUT OF THREE
+num1 = int(input("Enter 1st number:"))
+num2 = int(input("Enter 2nd number:"))
+num3 = int(input("Enter 3rd number:"))
+if num1 > num2 :
+    if num1 > num3 :
+        print("Largest number is :",num1)
+    else :
+        print("Largest number is:",num2)
+else :
+    print("Largest number is :",num3)
+    
