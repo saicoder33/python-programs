6fb762c4a9e05bbfe9afd61964b66fb4a1ecaf14
saicoder33 <saidighe731@gmail.com>
2026-01-14 22:17:11 +0530

    3rd program

---
+++ UNIT2PRG3.py
@@ -0,0 +1,14 @@
+#PYTHON PROGRAM TO CHECK WHETHER THE ENTERED ALPHABET IS VOWEL OR CONSONANT
+ch = input("Enter a character:")
+if ch in "aeiouAEIOU":
+    print("Its a vowel")
+else:
+    print("Its a consonant")
+
+#PYTHON PROGRAM TO CHECK WHETHER A NUMBER IS DIVISBLE BY 5
+num = float(input("Enter a number:"))
+if num % 2 ==0:
+    print("Number is divisible by 5")
+else:
+    print("Number is not divisble by 5")
+    
